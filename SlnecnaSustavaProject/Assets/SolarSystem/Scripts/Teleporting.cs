55a980726ce36b119c3df3a8b13f19b0541c7384
KosoScythe <47943056+KosoScythe@users.noreply.github.com>
2021-01-31 20:14:53 +0100

    Finall

--- SlnecnaSustavaProject/Assets/SolarSystem/Scripts/Teleporting.cs
+++ SlnecnaSustavaProject/Assets/SolarSystem/Scripts/Teleporting.cs
@@ -62,7 +62,7 @@ public class Teleporting : MonoBehaviour
     }
 
     private void recognizePlanet(string name){
-        if (name == "Sun") changeCamera(new Vector3(-369, 168, 50), new Vector3(2,-24,-2));
+        if (name == "Sun") changeCamera(new Vector3(-369, 168, 50), new Vector3(1,-34,0));
         else if (name == "Mercury") changeCamera(   new Vector3(-432, 168, 265), new Vector3(-5,-19, 0) );
         else if (name == "Venus") changeCamera(     new Vector3(-317, 169, 320), new Vector3(10,-22, 0) );
         else if (name == "Clouds") changeCamera(    new Vector3(-432, 169, 422), new Vector3(17,-23, 0) );
@@ -80,7 +80,6 @@ public class Teleporting : MonoBehaviour
 
         RaycastHit hit;
         Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
-        Debug.DrawRay(transform.position,forward, Color.green);
 
         if(Physics.Raycast(transform.position, forward, out hit)){
             timeCounter++;
@@ -88,36 +87,49 @@ public class Teleporting : MonoBehaviour
 
             if (timeCounter == 500){
                 //Ak idem prvy krat na planetu
-                if (name != currentFocus) {
+                if (hitObject != currentFocus) {
                     recognizePlanet(hitObject);
                     showText(hitObject);
                 }
                 //chcem zborazit iba modal
                 else {
                     //ak som v blizkosti planety, zobrazim modal
-                    float targetX = hit.collider.gameObject.transform.position.x;
-                    float targetY = hit.collider.gameObject.transform.position.y;
-                    float targetZ = hit.collider.gameObject.transform.position.z;
+                    /*print("CHECK IF I AM OUT OF SIGHT");
+                    float targetX = hit.collider.gameObject.transform.rotation.x;
+                    float targetY = hit.collider.gameObject.transform.rotation.y;
+                    float targetZ = hit.collider.gameObject.transform.rotation.z;
 
-                    float cameraX = Camera.main.transform.position.x;
-                    float cameraY = Camera.main.transform.position.y;
-                    float cameraZ = Camera.main.transform.position.z;
+                    float cameraX = Camera.main.transform.rotation.x;
+                    float cameraY = Camera.main.transform.rotation.y;
+                    float cameraZ = Camera.main.transform.rotation.z;
 
-                    int accuracy = 50;
+                    int accuracy = 5;
                     
-                    if ((cameraX <= targetX + accuracy && cameraX >= targetX - accuracy) &&
-                        (cameraY <= targetY + accuracy && cameraY >= targetY - accuracy) &&
-                        (cameraZ <= targetZ + accuracy && cameraZ >= targetZ - accuracy)){
+                    if ((cameraX <= 2 + accuracy && cameraX >= -2 - accuracy) &&
+                        (cameraY <= -24 + accuracy && cameraY >= -24 - accuracy) &&
+                        (cameraZ <= -2 + accuracy && cameraZ >= -2 - accuracy)){
+                            print("MA NeGgRo");
                             showText(hitObject);
-                        }
+                    }
+                    else{
+                        if (currentModal != null) currentModal.SetActive(false);
+                    }*/
                 }
             }
         }
         else {
-            if (currentModal != null){
-                //currentModal.SetActive(false);
-            }
             timeCounter = 0;
+        }
+        if (currentFocus != ""){
+        Vector3 screenPoint = Camera.main.WorldToViewportPoint(GameObject.Find(currentFocus).transform.position);
+        bool onScreen = screenPoint.z > 0 && screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
+
+        if (onScreen) {
+            currentModal.SetActive(true);
+        }
+        else {
+            currentModal.SetActive(false);
+        }
         }
     }
 }
